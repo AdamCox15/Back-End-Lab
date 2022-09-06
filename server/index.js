014c66a8b98ebcdf24ab061af3f2c33c2deda9c5
Adam Cox <adamcox.dev@gmail.com>
2022-09-06 16:56:18 -0600

    second commit

--- server/index.js
+++ server/index.js
@@ -7,8 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+
 app.get("/api/users", (req, res) => {
-    let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
     res.status(200).send(friends);
   });
 
@@ -22,5 +23,24 @@ app.get("/api/users", (req, res) => {
     const phrase = `<h3>It was ${temperature} today</h3>`;
     res.status(200).send(phrase);
   });
+
+  app.post("/api/users", (req, res) => {
+    console.log(req.body)
+    friends.push(req.body.test)
+    res.status(200).send(friends)
+});
+
+  // app.delete("/api/users", (req, res) => {
+  //   let delName = req.query.users
+  //   for (let i = 0; i < friends.length; i++) {
+  //     if (delName) {
+  //       console.log("Deleted user:" + delName)
+  //       res.status(200).send("User deleted.")
+  //     }
+  //   }
+  //   res.status(400).send("User not found.")
+  // });
+
+
 
 app.listen(4000, () => console.log('Sever is running on 4000'));
